6a91686e51f05360c6d4a393985047dadc2751bb
Andreas JungÂ <info@zopyx.com>
2017-01-16 17:52:57 +0100

    updated

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ version = '0.1.0'
 
 setup(name='zopyx.smashdocs',
       version=version,
-      description="Smashdocs API wrapper",
+      description="Integration of Python with the Smashdocs Partner API",
       long_description=open(os.path.join("docs", "source", "README.rst")).read() + "\n" +
       open(os.path.join("docs", "source", "HISTORY.rst")).read(),
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
           "Topic :: Software Development :: Libraries :: Python Modules",
       ],
       author='Andreas Jung',
