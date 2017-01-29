7759f11ee19bce9f77c7cd33eddd812882900013
Andreas Jung <info@zopyx.com>
2017-01-29 15:47:46 +0100

    fix

--- zopyx/smashdocs/scripts/sd_ls.py
+++ zopyx/smashdocs/scripts/sd_ls.py
@@ -23,6 +23,9 @@ client = Smashdocs(
 @click.option('--group', default=None, help='Smashdocs Group ID')
 def list_documents(user=None, group=None):
 
+    if not group and not user:
+        raise ValueError('You specify at least --group and/or --user')
+
     for x in client.get_documents(user_id=user, group_id=group):
         print(x)
 
