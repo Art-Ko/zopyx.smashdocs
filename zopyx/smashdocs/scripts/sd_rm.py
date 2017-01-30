f09b2eebf9cbaf89efd14b78a1c4ac5f22e277f5
Andreas JungÂ <info@zopyx.com>
2017-01-30 15:07:14 +0100

    updated

--- zopyx/smashdocs/scripts/sd_rm.py
+++ zopyx/smashdocs/scripts/sd_rm.py
@@ -9,10 +9,10 @@
 import os
 import click
 
-from zopyx.smashdocs.api import Smashdocs
+from zopyx.smashdocs import api
 
 
-client = Smashdocs(
+client = api.Smashdocs(
         partner_url=os.environ['SMASHDOCS_PARTNER_URL'] ,
         client_id=os.environ['SMASHDOCS_CLIENT_ID'] ,
         client_key=os.environ['SMASHDOCS_CLIENT_KEY'])
@@ -23,8 +23,11 @@ client = Smashdocs(
 def remove_documents(user=None, documents=[]):
 
     for document_id in documents:
-        client.delete_document(document_id)
-        print('deleted {}'.format(document_id))
+        try:
+            client.delete_document(document_id)
+            print('deleted {}'.format(document_id))
+        except api.DeletionError as e:
+            print('ERROR: {}'.format(e))
 
 if __name__ == '__main__':
     remove_documents()
